c2498b513431958d275f1af05e29aaef8855284e
edgekillerfr <edgekillerfr@live.fr>
2014-10-25 17:31:15 +0200

    Optimizations

--- SharpMail/Class1.cs
+++ SharpMail/Class1.cs
@@ -10,47 +10,32 @@ namespace SharpMail
 {
     public static class Mail
     {
-        public static void SendMail(string SMTP, string FROM, string TO, string SUBJECT, string BODY, string USERNAME, string PASSWORD)
+        public static void SendMail(string SMTP, string FROM, string TO, string SUBJECT, string BODY, string USERNAME, string PASSWORD, string MSGBOXSUCCESS = "")
         {
-            try
+            using (SmtpClient SmtpServer = new SmtpClient(SMTP))
             {
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient(SMTP);
-                mail.From = new MailAddress(FROM);
-                mail.To.Add(TO);
-                mail.Subject = SUBJECT;
-                mail.Body = BODY;
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
-                SmtpServer.EnableSsl = true;
-                SmtpServer.Send(mail);
-                SmtpServer.Dispose();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-        }
-        public static void SendMail(string SMTP, string FROM, string TO, string SUBJECT, string BODY, string USERNAME, string PASSWORD, string MSGBOXSUCCESS)
-        {
-            try
-            {
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient(SMTP);
-                mail.From = new MailAddress(FROM);
-                mail.To.Add(TO);
-                mail.Subject = SUBJECT;
-                mail.Body = BODY;
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
-                SmtpServer.EnableSsl = true;
-                SmtpServer.Send(mail);
-                SmtpServer.Dispose();
-                MessageBox.Show(MSGBOXSUCCESS);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.From = new MailAddress(FROM);
+                    mail.To.Add(TO);
+                    mail.Subject = SUBJECT;
+                    mail.Body = BODY;
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(USERNAME, PASSWORD);
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mail);
+                    if (MSGBOXSUCCESS != "")
+                        MessageBox.Show(MSGBOXSUCCESS);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    SmtpServer.Dispose();
+                }
             }
         }
 
